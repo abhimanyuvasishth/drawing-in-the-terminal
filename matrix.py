e12e38a9f288e983b31afdb753d14e5f031c8225
abhimanyu <abhimanyu.vasishth@gmail.com>
2016-09-19 16:51:00 +0400

    workshop+test code

--- matrix.py
+++ matrix.py
@@ -1,8 +1,8 @@
 # builtin packages
 from sys import stdout
+import os
 from time import sleep
 import random
-import os
 
 #additional packages > install using pip install <NAMEOFPACKAGE>
 import noise
@@ -13,24 +13,21 @@ interval = 0.001
 term = Terminal()
 width = term.width
 height = term.height
+unit = 5
 
-possible_locations_columns = []
-column_spacing = 7
-
-for x in range (0, int(width/column_spacing)):
-    possible_locations_columns.append(x*column_spacing)
-
-# get_rid_of_top_line = ""
+cols = []
 os.system("clear")
+
+for x in range(0,int(width/unit)):
+	cols.append(x*unit)
 
 def random_move_drawing():
     while True:
-        with term.location(possible_locations_columns[random.randint(0, len(possible_locations_columns)-1)], random.randint(1, height-2)):
-            print(term.green+str(random.randint(0, 9)))
+        with term.location(cols[random.randint(0,len(cols)-1)], random.randint(0, height-2)):
+            print(term.green+str(random.randint(0,9)))
             term.location()
-
 
 try:
     random_move_drawing()
 except KeyboardInterrupt:
-    print(term.green+"bye!")
+    print("bye!")
--- test.py
+++ test.py
@@ -2,18 +2,15 @@ from time import sleep
 from sys import stdout
 import random
 import noise
-
 from blessings import Terminal
 
 term = Terminal()
 width = term.width
 height = term.height
 
+print(width)
 noise_step = 0
 
-
-#helper function
-#credit due to Adam Luchjenbroers on StackOverflow <3
 def map(value, leftMin, leftMax, rightMin, rightMax):
     # Figure out how 'wide' each range is
     leftSpan = leftMax - leftMin
@@ -25,23 +22,22 @@ def map(value, leftMin, leftMax, rightMin, rightMax):
     # Convert the 0-1 range into a value in the right range.
     return rightMin + (valueScaled * rightSpan)
 
-
 while True:
-    my_line = ""
-    noise_value = noise.pnoise1(noise_step, 1)
-    mapped_value = int(map(noise_value, -1, 1, 0, width))
-    mapped_value_width = int(map(noise_value*10, -5, 5, 0, 10))
+	my_line = ""
+	noise_value = noise.pnoise1(noise_step,1)
+	mapped_value = int(map(noise.pnoise1(noise_step,1), -1,1,0,width))
+	mapped_value_width = int(map(noise.pnoise1(noise_step,1), -1,1,2,10))
 
+	# for x in range(0,mapped_value):
+	for x in range(0,width):
+		# my_line += "o"
 
-    for x in range (0, width):
-        if x > mapped_value-mapped_value_width and x < mapped_value+mapped_value_width:
-            my_line += "|"
-        else:
-            my_line += " "
+		if x > mapped_value-mapped_value_width and x < mapped_value+mapped_value_width:
+			my_line += ' '
+		else:
+			my_line += ' '
 
-    print(my_line)
-
-
-    noise_step += 0.01
-    stdout.flush()
-    sleep(0.01)
+	print(term.on_color(random.randint(0,10)) + my_line + '\n')
+	noise_step += 0.01
+	stdout.flush()
+	sleep(0.01)
